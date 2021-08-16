e189dfa0d643b27ae75c2682848051f42add60a0
Glitch (glitch-hello-website) <none>
2021-08-16 17:48:05 +0000

    🖇📫 Checkpoint ./script.js:34106937/451 ./index.html:34106937/370 ./style.css:34106937/8375

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 fetch("https://api.github.com/users/MystPi/repos").then(data => data.json()).then(data => {
-  document.getElementById("progress-repos").style.width = data.length / 100 + "%";
-})
+  document.getElementById("progress-repos").style.width = data.length / 20 * 100 + "%";
+  document.getElementById("number").innerHTML = data.length + " / 20";
+});
