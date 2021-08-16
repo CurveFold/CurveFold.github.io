aaa7359a7c07124d76ae60378c1396695bb525af
Glitch (glitch-hello-website) <none>
2021-08-16 19:26:02 +0000

    🚧💃 Checkpoint ./index.html:34106937/1831 ./script.js:34106937/537 ./style.css:34106937/16

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 fetch("https://api.github.com/users/MystPi/repos").then(data => data.json()).then(data => {
   document.getElementById("progress-repos").style.width = data.length / 20 * 100 + "%";
-  document.getElementById("number").innerHTML = data.length + " / 20";
+  document.getElementById("repo-number").innerHTML = data.length + " / 20";
+  let list = document.getElementById("repos");
+  for (let i = 0; i < data.length; i++) {
+    list.innerHTML += `<li><a href="${data[i].html_url}">${data[i].name}</a></li>`
+  }
+  fetch("https://api.scratch.mit.edu/users/NFlex23/projects").then(data => data.json()).then(data => {
+    document.getElementById("progress-projects").style.width = data.length / 100 * 100 + "%";
+    document.getElementById("project-number").innerHTML
+  })
 });
