05b21f3a868bfd8cda69a3221c62fda2d2400b3d
CurveFold <69720319+CurveFold@users.noreply.github.com>
2021-10-29 16:29:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-fetch("https://api.github.com/users/Iegendi/repos").then(data => data.json()).then(data => {
+fetch("https://api.github.com/users/CurveFold/repos").then(data => data.json()).then(data => {
   document.getElementById("progress-repos").style.width = data.length / 20 * 100 + "%";
   document.getElementById("repo-number").innerHTML = data.length + " / 20";
   let list = document.getElementById("repos");
   for (let i = 0; i < data.length; i++) {
     list.innerHTML += `<li><a href="${data[i].html_url}">${data[i].name}</a></li>`;
   }
-  fetch("https://api.github.com/users/Iegendi").then(about => about.json()).then(about => {
+  fetch("https://api.github.com/users/CurveFold").then(about => about.json()).then(about => {
     document.getElementById("about").innerHTML = about.bio;
   });
 });
